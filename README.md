c9156037d76b960b1b814eda346348588905a1fb
akihiro_0228 <nano.universe.0228@gmail.com>
2014-06-05 00:56:21 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::HidamariSketch - It's new $module
+
+# SYNOPSIS
+
+    use Acme::HidamariSketch;
+
+# DESCRIPTION
+
+Acme::HidamariSketch is ...
+
+# LICENSE
+
+Copyright (C) akihiro\_0228.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+akihiro\_0228 <nano.universe.0228@gmail.com>
---
+++ lib/Acme/HidamariSketch.pm
@@ -0,0 +1,39 @@
+package Acme::HidamariSketch;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::HidamariSketch - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::HidamariSketch;
+
+=head1 DESCRIPTION
+
+Acme::HidamariSketch is ...
+
+=head1 LICENSE
+
+Copyright (C) akihiro_0228.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+akihiro_0228 E<lt>nano.universe.0228@gmail.comE<gt>
+
+=cut
+
